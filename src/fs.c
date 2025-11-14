8d8bc941dc2495a857621fd5bb234f5017036ca4
Xukun Li <ximenalee5@gmail.com>
2025-11-13 23:07:17 -0500

    Format code

--- src/fs.c
+++ src/fs.c
@@ -6,8 +6,8 @@
 #include "main.h"
 
 #include "fs.h"
-#include "log.h"
 #include "littlefs_shim.h"
+#include "log.h"
 
 #define MAX_FILE_PER_DIR 1000
 
@@ -19,86 +19,84 @@ uint32_t page_counter = 0;
 
 // configuration of the filesystem is provided by this struct
 const struct lfs_config cfg = {
-    // block device operations
-    .read = sd_read,
-    .prog = sd_write,
-    .erase = sd_erase,
-    .sync = sd_sync,
+	// block device operations
+	.read = sd_read,
+	.prog = sd_write,
+	.erase = sd_erase,
+	.sync = sd_sync,
 
-    // block device configuratio4n
-    .read_size = 16,
-    .prog_size = 16,
-    .block_size = 4096,
-    .block_count = 128,
-    .cache_size = 16,
-    .lookahead_size = 16,
-    .block_cycles = 500,
+	// block device configuratio4n
+	.read_size = 16,
+	.prog_size = 16,
+	.block_size = 4096,
+	.block_count = 128,
+	.cache_size = 16,
+	.lookahead_size = 16,
+	.block_cycles = 500,
 };
 
 static void fs_new_file(void) {
-    //    unsigned int retval;
+	//    unsigned int retval;
 
-    // Create directory as nessary
-    if ((index_counter % MAX_FILE_PER_DIR) == 0) {
-        char dir_name[100];
-        sprintf(dir_name, "dir_%04lu", index_counter / MAX_FILE_PER_DIR);
-        lfs_mkdir(&lfs, dir_name);
-    }
+	// Create directory as nessary
+	if ((index_counter % MAX_FILE_PER_DIR) == 0) {
+		char dir_name[100];
+		sprintf(dir_name, "dir_%04lu", index_counter / MAX_FILE_PER_DIR);
+		lfs_mkdir(&lfs, dir_name);
+	}
 
-    // Choose file name
-    char log_filename[100];
-    sprintf(
-        log_filename,
-        "dir_%04lu/log_%04lu.bin",
-        index_counter / MAX_FILE_PER_DIR,
-        index_counter % MAX_FILE_PER_DIR
-    );
+	// Choose file name
+	char log_filename[100];
+	sprintf(log_filename,
+			"dir_%04lu/log_%04lu.bin",
+			index_counter / MAX_FILE_PER_DIR,
+			index_counter % MAX_FILE_PER_DIR);
 
-    ++index_counter;
+	++index_counter;
 
-    // Update counter file
-    lfs_file_t counter_file;
-    lfs_file_open(&lfs, &counter_file, "counter.bin", LFS_O_WRONLY | LFS_O_CREAT | LFS_O_TRUNC);
-    lfs_file_write(&lfs, &counter_file, &index_counter, sizeof(index_counter));
-    lfs_file_close(&lfs, &counter_file);
+	// Update counter file
+	lfs_file_t counter_file;
+	lfs_file_open(&lfs, &counter_file, "counter.bin", LFS_O_WRONLY | LFS_O_CREAT | LFS_O_TRUNC);
+	lfs_file_write(&lfs, &counter_file, &index_counter, sizeof(index_counter));
+	lfs_file_close(&lfs, &counter_file);
 
-    if (lfs_file_open(&lfs, &logfile, log_filename, LFS_O_WRONLY | LFS_O_CREAT | LFS_O_EXCL) != 0) {
-    }
+	if (lfs_file_open(&lfs, &logfile, log_filename, LFS_O_WRONLY | LFS_O_CREAT | LFS_O_EXCL) != 0) {
+	}
 
-    page_counter = 0;
+	page_counter = 0;
 }
 
 w_status_t fs_init(void) {
-    //    unsigned int retval;
-    if (lfs_format(&lfs, &cfg)) {
-        return W_FAILURE;
-    }
+	//    unsigned int retval;
+	if (lfs_format(&lfs, &cfg)) {
+		return W_FAILURE;
+	}
 
-    if (lfs_mount(&lfs, &cfg) != 0) {
-        return W_IO_ERROR;
-    }
+	if (lfs_mount(&lfs, &cfg) != 0) {
+		return W_IO_ERROR;
+	}
 
-    // Read the file count counter
-    lfs_file_t counter_file;
-    if (lfs_file_open(&lfs, &counter_file, "counter.bin", LFS_O_RDONLY) == 0) {
-        lfs_file_read(&lfs, &counter_file, &index_counter, sizeof(index_counter));
-    }
-    lfs_file_close(&lfs, &counter_file);
+	// Read the file count counter
+	lfs_file_t counter_file;
+	if (lfs_file_open(&lfs, &counter_file, "counter.bin", LFS_O_RDONLY) == 0) {
+		lfs_file_read(&lfs, &counter_file, &index_counter, sizeof(index_counter));
+	}
+	lfs_file_close(&lfs, &counter_file);
 
-    fs_new_file();
+	fs_new_file();
 
-    return W_SUCCESS;
+	return W_SUCCESS;
 }
 
 void fs_write_page(const uint8_t *page) {
-    //    unsigned int retval;
-    if (lfs_file_write(&lfs, &logfile, page, PAGE_SIZE) != 0) {}
-    ++page_counter;
+	//    unsigned int retval;
+	if (lfs_file_write(&lfs, &logfile, page, PAGE_SIZE) != 0) {}
+	++page_counter;
 
-    if (page_counter >= MAX_FILE_SIZE_PAGES) {
-        lfs_file_close(&lfs, &logfile);
-        fs_new_file();
-    } else {
-        lfs_file_sync(&lfs, &logfile);
-    }
+	if (page_counter >= MAX_FILE_SIZE_PAGES) {
+		lfs_file_close(&lfs, &logfile);
+		fs_new_file();
+	} else {
+		lfs_file_sync(&lfs, &logfile);
+	}
 }
