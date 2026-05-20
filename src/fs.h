e352d2b698e20c2eafca2eae2b9c04f5bd954fac
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2026-05-20 19:31:02 -0400

    Add partial support for sending SD logging status

--- src/fs.h
+++ src/fs.h
@@ -5,5 +5,10 @@
 
 w_status_t fs_init(void);
 void fs_write_page(const uint8_t *page);
+w_status_t status_report(void);
+uint32_t fs_get_log_written_size(void);
+uint32_t fs_get_sd_log_file_name(void);
+uint32_t fs_get_sd_used(void);
+uint32_t fs_get_sd_free(void);
 
 #endif
--- src/health_check.c
+++ src/health_check.c
@@ -4,16 +4,15 @@
 #include "health_check.h"
 #include "platform.h"
 
-extern ADC_HandleTypeDef hadc1;
-extern ADC_HandleTypeDef hadc2;
+extern ADC_HandleTypeDef hadc1; // Vin Current Sense
+extern ADC_HandleTypeDef hadc2; // Vin Voltage Sense
 
-#define BATT_CURR_SCALAR_TIMES                                                                     \
-	2 // ADC / 4096 * 2.8V / 100V/V / 0.03 Ohm * 1000mA/A = ADC * 0.227 = ADC * 2 / 9
-#define BATT_CURR_SCALAR_DIV 9
-#define BATTERY_CURRENT_THRESHOLD 400 // Should be even less when running without OV5640
+// ADC / 4096 * 2.5V / 100V/V / 0.03 Ohm * 1000mA/A = ADC * 0.203mA = ADC / 5
+#define BATT_CURR_SCALAR_DIV 5
+#define BATTERY_CURRENT_THRESHOLD 100
 
-#define BATT_VOLT_SCALAR_TIMES 17 // ADC / 4096 * 2.8V * 5 * 1000 mV/V = ADC * 3.417
-#define BATT_VOLT_SCALAR_DIV 5
+// ADC / 4096 * 2.5V * 5 * 1000 mV/V = ADC * 3.051mV
+#define BATT_VOLT_SCALAR_MUL 3
 #define BATT_VOLT_LOW_THRESHOLD 8000
 #define BATT_VOLT_HIGH_THRESHOLD 13000 // ADC will saturate by then
 
@@ -25,20 +24,20 @@ uint32_t health_check(void) {
 	HAL_ADC_Start(&hadc1);
 	HAL_ADC_PollForConversion(&hadc1, 500);
 	uint16_t adcval = HAL_ADC_GetValue(&hadc1); // 12-bit ADC
-	uint16_t battery_current_mA = adcval * BATT_CURR_SCALAR_TIMES / BATT_CURR_SCALAR_DIV;
+	uint16_t battery_current_mA = adcval / BATT_CURR_SCALAR_DIV;
 
 	if (battery_current_mA > BATTERY_CURRENT_THRESHOLD) {
 		status_msg_general_status |= (1 << E_12V_OVER_CURRENT_OFFSET);
 	}
 
-	build_analog_data_msg(PRIO_LOW, millis(), SENSOR_12V_CURR, battery_current_mA, &msg);
-	can_send(&msg);
+	build_analog_sensor_16bit_msg(PRIO_LOW, millis(), SENSOR_12V_CURR, battery_current_mA, &msg);
+	stm32h7_can_send(&msg);
 
 	// 12V Voltage Check
 	HAL_ADC_Start(&hadc2);
 	HAL_ADC_PollForConversion(&hadc2, 500);
 	adcval = HAL_ADC_GetValue(&hadc2); // 12-bit ADC
-	uint16_t battery_voltage_mV = adcval * BATT_VOLT_SCALAR_TIMES / BATT_VOLT_SCALAR_DIV;
+	uint16_t battery_voltage_mV = adcval * BATT_VOLT_SCALAR_MUL;
 
 	if (battery_voltage_mV > BATT_VOLT_HIGH_THRESHOLD) {
 		status_msg_general_status |= (1 << E_12V_OVER_VOLTAGE_OFFSET);
@@ -48,8 +47,8 @@ uint32_t health_check(void) {
 		status_msg_general_status |= (1 << E_12V_UNDER_VOLTAGE_OFFSET);
 	}
 
-	build_analog_data_msg(PRIO_LOW, millis(), SENSOR_12V_VOLT, battery_voltage_mV, &msg);
-	can_send(&msg);
+	build_analog_sensor_16bit_msg(PRIO_LOW, millis(), SENSOR_12V_VOLT, battery_voltage_mV, &msg);
+	stm32h7_can_send(&msg);
 
 	return status_msg_general_status;
 }
