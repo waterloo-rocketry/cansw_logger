72f5ff81e6bae8c7418efe39e6c3b2e00e76924d
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2026-05-20 23:48:49 -0400

    Fix warm reset

--- src/mx25l25645g.c
+++ src/mx25l25645g.c
@@ -11,7 +11,7 @@ void mx25l25645g_init(void) {
 
 	// Instruction Fields
 	commandtypedef.Instruction = 0; // RDID
-	commandtypedef.InstructionMode = QSPI_INSTRUCTION_1_LINE;
+	commandtypedef.InstructionMode = QSPI_INSTRUCTION_4_LINES;
 
 	// Address Fields
 	commandtypedef.Address = 0;
@@ -24,8 +24,8 @@ void mx25l25645g_init(void) {
 	commandtypedef.AlternateByteMode = QSPI_ALTERNATE_BYTES_NONE;
 
 	// Data Fields
-	commandtypedef.DataMode = QSPI_DATA_1_LINE;
-	commandtypedef.NbData = 3;
+	commandtypedef.DataMode = QSPI_DATA_NONE;
+	commandtypedef.NbData = 0;
 
 	// Other Fields
 	commandtypedef.DummyCycles = 0;
@@ -33,9 +33,23 @@ void mx25l25645g_init(void) {
 	commandtypedef.DdrHoldHalfCycle = 0;
 	commandtypedef.SIOOMode = QSPI_SIOO_INST_EVERY_CMD;
 
+	// Exit QuadSPI(QPI) Mode, in case of a warm reset
+	commandtypedef.Instruction = 0xf5; // RSTQIO
+	HAL_QSPI_Command(&hqspi, &commandtypedef, HAL_QSPI_TIMEOUT_DEFAULT_VALUE);
+
+	// Reset flash chip(two commands)
+	commandtypedef.Instruction = 0x66; // RSTEN
+	commandtypedef.InstructionMode = QSPI_INSTRUCTION_1_LINE;
+	HAL_QSPI_Command(&hqspi, &commandtypedef, HAL_QSPI_TIMEOUT_DEFAULT_VALUE);
+
+	commandtypedef.Instruction = 0x99; // RST
+	HAL_QSPI_Command(&hqspi, &commandtypedef, HAL_QSPI_TIMEOUT_DEFAULT_VALUE);
+
 	// Read JEDEC ID in SPI Mode
 	uint8_t spi_id[3];
 	commandtypedef.Instruction = 0x9f; // RDID
+	commandtypedef.DataMode = QSPI_DATA_1_LINE;
+	commandtypedef.NbData = 3;
 	HAL_QSPI_Command(&hqspi, &commandtypedef, HAL_QSPI_TIMEOUT_DEFAULT_VALUE);
 	HAL_QSPI_Receive(&hqspi, spi_id, HAL_QSPI_TIMEOUT_DEFAULT_VALUE);
 
