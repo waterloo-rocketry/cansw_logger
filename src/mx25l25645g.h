27381c9d40654e0567cfb0209c2f981a2ab8e9d5
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2026-05-30 20:25:29 -0400

    Implement read and write function

--- src/mx25l25645g.h
+++ src/mx25l25645g.h
@@ -1,6 +1,12 @@
 #ifndef MX25L25645G_H
 #define MX25L25645G_H
 
+#include <stdint.h>
+
 void mx25l25645g_init(void);
+void mx25l25645g_erase(uint32_t address);
+void mx25l25645g_erase_chip(void);
+void mx25l25645g_write(uint32_t address, uint32_t npages, const uint8_t *buffer);
+void mx25l25645g_read(uint32_t address, uint32_t npages, uint8_t *buffer);
 
 #endif
